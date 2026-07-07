95c4812b8553aee320142ebcc5e27baf4071f866
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-07 22:31:39 +0700

    fixed

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -1,26 +1,19 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-// import MyComponent from "./components/MyComponent";
+// import { useDispatch, useSelector } from "react-redux";
+// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import MyComponent from "./components/MyComponent";
+import React from "react";
 
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is a simple React Redux counter application. You can
-          increase or decrease the count using the buttons below.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Xin chào React !
+        <MyComponent />
+      </div>
+    );
+  }
+}
 
 export default App;
