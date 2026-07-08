242eac04ab03703a5d73d8d8f2667e7903bf77e5
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-08 09:31:19 +0700

    DOM Events

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -8,12 +8,23 @@ class MyComponent extends React.Component {
     age: 25,
   };
 
+  handleClick(event) {
+    console.log("click");
+    // console.log(event);
+  }
+
+  handleOnMoverOver(event) {
+    console.log(event.pageX, event.pageY);
+  }
+
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name}, I am {this.state.age} years old and I live
         in {this.state.address}.
+        <button onClick={this.handleClick}>Click me!</button>
+        <button onMouseOver={this.handleOnMoverOver}>Hover me!</button>
       </div>
     );
   }
