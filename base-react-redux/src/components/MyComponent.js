74fd715bdc3d9cce80eb23330ad3b47eda0d1b87
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-08 17:10:47 +0700

    Nesting Components

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -1,57 +1,13 @@
 //Class component
 //function component
 import React from "react";
+import UserInfo from "./UserInfo";
 class MyComponent extends React.Component {
-  state = {
-    name: "Thịnh Nguyễn",
-    address: "Hà Nội",
-    age: 25,
-  };
-
-  handleClick(event) {
-    console.log("click");
-    this.setState({
-      name: "Thịnh",
-    });
-  }
-
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-    console.log(event.target.value);
-  };
-
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    alert("submit");
-    console.log(this.state);
-  };
-
   //JSX
   render() {
     return (
       <div>
-        My name is {this.state.name}, I am {this.state.age} years old and I live
-        in {this.state.address}.
-        <button
-          onClick={(event) => {
-            this.handleClick(event);
-          }}
-        >
-          Click me!
-        </button>
-        <form
-          onSubmit={(event) => {
-            this.handleOnSubmit(event);
-          }}
-        >
-          <input
-            type="text"
-            onChange={(event) => this.handleOnChangeInput(event)}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfo />
       </div>
     );
   }
