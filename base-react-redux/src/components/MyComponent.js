a2d51d2764877edc093e862b80c64648475b3597
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-09 09:49:25 +0700

    Outputing list

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -4,13 +4,23 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listUsers: [
+        { id: 1, name: "Thịnh Nguyễn", age: 25 },
+        { id: 2, name: "Phú Thịnh", age: 30 },
+        { id: 3, name: "Eric", age: 36 },
+      ],
+    };
+  }
   //JSX
   render() {
     return (
       <div>
         <UserInfo />
         <br />
-        <DisplayInfo name="Phú Thịnh" age={30} />
+        <DisplayInfo listUser={this.state.listUsers} />
       </div>
     );
   }
