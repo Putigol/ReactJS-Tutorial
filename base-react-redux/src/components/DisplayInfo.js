1082bf7cb4c420b3cf9380a40e9e960f5b333244
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-10 23:51:32 +0700

    Conditional Output

--- base-react-redux/src/components/DisplayInfo.js
+++ base-react-redux/src/components/DisplayInfo.js
@@ -1,22 +1,53 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShow: true,
+  };
+
+  handleShowHide = () => {
+    alert("Show list users");
+  };
   render() {
     //Destructuring
     const { listUser } = this.props;
     return (
-      //
       <div>
-        <div>DisplayInfo</div>
-        {listUser.map((user) => (
-          <div key={user.id}>
-            <div>ID: {user.id}</div>
-            <div>Name: {user.name}</div>
-            <div>Age: {user.age}</div>
-            <hr />
+        <div>
+          <span onClick={() => this.handleShowHide()}> Hide list users: </span>
+        </div>
+        {true && (
+          <div>
+            {listUser.map((user, index) => {
+              console.log(">>> check user: ", user);
+              return (
+                <div key={user.id} className={user.age > 30 ? "red" : "green"}>
+                  <div>My name's {user.name}</div>
+                  <div>My age's {user.age}</div>
+                  <hr />
+                </div>
+              );
+              //Dấu + trước biến để ép kiểu dữ liệu sang number
+              //   if (+user.age > 30) {
+              //     return (
+              //       <div key={user.id} className="red">
+              //         <div>My name's {user.name}</div>
+              //         <div>My age's {user.age}</div>
+              //         <hr />
+              //       </div>
+              //     );
+              //   } else {
+              // return (
+              //   <div key={user.id} className="green">
+              //     <div>My name's {user.name}</div>
+              //     <div>My age's {user.age}</div>
+              //     <hr />
+              //   </div>
+              // );
+              //   }
+            })}
           </div>
-        ))}
-        <hr />
+        )}
       </div>
     );
   }
